87592a5ad3c0950687c778d7b378a716472ac55a
Paweł Cichowski <pcichowski@pcichowski.com>
2024-04-15 10:41:51 +0200

    resnet34/version21 with low lr
    Signed-off-by: Paweł Cichowski <pcichowski@pcichowski.com>

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ resnet18.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 # summary(resnet18, (3, 224, 224))
 
 # resnet34 = ResNet34(3, outputs=5)
-resnet34 = ResNet(in_channels=3, repeat=[3, 4, 6, 3], use_bottleneck=False, outputs=5, optimizer_lr=0.001, scheduler_step=25, scheduler_gamma=0.1)
+resnet34 = ResNet(in_channels=3, repeat=[3, 4, 6, 3], use_bottleneck=False, outputs=5, optimizer_lr=0.00001, scheduler_step=25, scheduler_gamma=0.1)
 # resnet34 = ResNet34.load_from_checkpoint("tb_logs/resnet34/version_14/checkpoints/epoch=49-step=4400.ckpt", in_channels=3, outputs=5)
 resnet34.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
 
@@ -52,3 +52,9 @@ trainer = pl.Trainer(
 torch.set_float32_matmul_precision('medium')
 
 trainer.fit(resnet34, data_module)
+
+# tuner = Tuner(trainer)
+# lr_finder = tuner.lr_find(resnet34, data_module, attr_name='optimizer_lr')
+#
+# fig = lr_finder.plot(suggest=True)
+# fig.show()
